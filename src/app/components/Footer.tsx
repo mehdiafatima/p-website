170e6811f71a6122e2d7f1789715edb6bdbb18c6
Mehdia <mehdiaff9876@gmail.com>
2024-11-19 16:06:23 +0500

    changes

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="bg-black text-white py-6">
       <div className="max-w-6xl mx-auto text-center">
         <p className="text-sm mb-2">
-          &copy; {new Date().getFullYear()} Your Name. All Rights Reserved.
+          &copy; {new Date().getFullYear()} Mehdia Fatima Faizi. All Rights Reserved.
         </p>
         <p className="text-sm mb-2">Thank you for visiting my site!</p>
         <p className="text-sm">Contact me at: <a href="mehdiaff9876@gmail.com" className="text-purple-500 hover:text-purple-300">mehdiaff9876@gmail.com</a></p>
